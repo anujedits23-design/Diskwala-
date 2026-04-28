02664bdb7ec85809b32b8f9a2a439f55fb426864
anujedits23-design <anujedits23@gmail.com>
2026-04-28 21:03:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
-API_ID = 34446649
-API_HASH = "8dc570c08d8e35e88fb9bfc73c65d7fa"
-BOT_TOKEN = "8602719743:AAFu2J4o6UedCbrVnDdcCvn5DITPuzNEZZw"
+import os
 
-BASE_URL = "https://diskwala-i3qy.onrender.com"
-
+API_ID = int(os.environ.get("API_ID", "34446649"))
+API_HASH = os.environ.get("API_HASH", "8dc570c08d8e35e88fb9bfc73c65d7fa")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8602719743:AAFu2J4o6UedCbrVnDdcCvn5DITPuzNEZZw")
+BASE_URL = os.environ.get("BASE_URL", "https://diskwala-i3qy.onrender.com")
+SECRET_KEY = os.environ.get("SECRET_KEY", "diskwala-ultra-default-secret-change-me")
 LINK_EXPIRY = 172800
 MAX_UPLOAD_SIZE = 100 * 1024 * 1024
